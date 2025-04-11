088ff7bf83fcdd5bda86c4cf12dfb85994e9589b
NCMPAutomation <netconncmp@netcon.in>
2025-04-11 13:03:25 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,5 +1,8 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "C:\Scripts\TN School\TN_School_Script1.ps1"
-python "C:\Scripts\TN School\TN_School_Script2.py"
+set currentPath=%cd%
+echo The current path is: %currentPath%
+cd %currentPath%
+powershell -ExecutionPolicy Bypass -File "TN_School_Script1.ps1"
+python "TN_School_Script2.py"
 echo Done!
 pause
